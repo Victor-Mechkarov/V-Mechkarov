4541d96c6cf15955633e8f6c5be20d2e4b1a0415
Victor Mechkarov <v.mechkarov@icloud.com>
2026-04-04 13:19:02 +0300

    Delete lab_OOP/xfiles.py

--- lab_OOP/xfiles.py
+++
@@ -1,8 +0,0 @@
-
-import pandas as pd
-
-X = pd.read_excel('ball.xlsx')
-pd.set_option('display.max_columns', None, 'display.width', None, 'display.max_rows', None)
-
-dfr = pd.DataFrame(X)
-print(dfr)
